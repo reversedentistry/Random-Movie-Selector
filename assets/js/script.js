4078d656697f324855d521b0b23dcebee21ca23b
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-05-17 20:43:45 -0700

    Cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,12 @@ getRandomNumber();
 
 
 // two functions to fetch from the movie and pokemon apis respectively
-function getRandomMovie(){
+function getRandomMovie() {
   fetch(imdburl)
-    .then(function (response){
+    .then(function (response) {
       return response.json();
     })
-    .then(function (data){
+    .then(function (data) {
       getRandomNumber();
       randomMovieDiv.innerHTML = "Movie Name: " + data.items[randomNumber].title + "<br /> Ranking: " + data.items[randomNumber].rank + "<br /> IMDB Rating: " + data.items[randomNumber].imDbRating
       poster.setAttribute("src", data.items[randomNumber].image)
@@ -29,65 +29,63 @@ function getRandomMovie(){
 // funtion to grab a random number
 function getRandomNumber() {
   randomNumber = Math.floor(Math.random() * 249);
-  }
+}
 
 // Event listeners for the two buttons to get random movie or random pokemon
 randomBtn.addEventListener("click", getRandomMovie);
 // randomBtnPokemon.addEventListener("click", getRandomPokemon)
 
-let searchBtn = document.querySelector("#search-btn"); 
+let searchBtn = document.querySelector("#search-btn");
 let moviePosterEl = document.querySelector("#movie-poster");
-let movDetailsEl = document.querySelector("#movie-details"); 
+let movDetailsEl = document.querySelector("#movie-details");
 
 searchBtn.addEventListener("click", search);
 
 
 function search() {
-    let movieSearch = document.querySelector("#title-search").value;    
-    fetch('http://www.omdbapi.com/?apikey=3649a4e3&t=' + movieSearch)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-            console.log(data); 
-            displayMovie(data);
-        })
-        .catch(err => console.error(err));
+  let movieSearch = document.querySelector("#title-search").value;
+  fetch('http://www.omdbapi.com/?apikey=3649a4e3&t=' + movieSearch)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      displayMovie(data);
+    })
+    .catch(err => console.error(err));
 }
 
 function displayMovie(movieData) {
   let movieTitle = document.createElement("h3");
-  movieTitle.textContent = movieData.Title; 
-  console.log(movieTitle); 
-  movDetailsEl.append(movieTitle); 
-  
-  let moviePoster = document.createElement("img"); 
+  movieTitle.textContent = movieData.Title;
+  movDetailsEl.append(movieTitle);
+
+  let moviePoster = document.createElement("img");
   moviePoster.src = movieData.Poster;
-  console.log(movieData.Poster); 
-  moviePosterEl.append(moviePoster); 
-  
-  let movieDirector = document.createElement("p"); 
-  movieDirector.textContent = "Directed by: " + movieData.Director; 
-  movDetailsEl.append(movieDirector); 
-  
-  let movieRelease = document.createElement("p"); 
-  movieRelease.textContent = "Released: " + movieData.Released; 
-  movDetailsEl.append(movieRelease); 
+  moviePosterEl.append(moviePoster);
 
-  let moviePlot = document.createElement("p"); 
-  moviePlot.textContent = "Plot: " + '"' + movieData.Plot + '"'; 
-  movDetailsEl.append(moviePlot); 
+  let movieDirector = document.createElement("p");
+  movieDirector.textContent = "Directed by: " + movieData.Director;
+  movDetailsEl.append(movieDirector);
 
-  let movieCast = document.createElement("p"); 
-  movieCast.textContent = "Cast: " + movieData.Actors; 
-  movDetailsEl.append(movieCast); 
+  let movieRelease = document.createElement("p");
+  movieRelease.textContent = "Released: " + movieData.Released;
+  movDetailsEl.append(movieRelease);
 
-  let movieRating = document.createElement("p"); 
-  movieRating.textContent = "Rated: " + movieData.Rated; 
-  movDetailsEl.append(movieRating); 
+  let moviePlot = document.createElement("p");
+  moviePlot.textContent = "Plot: " + '"' + movieData.Plot + '"';
+  movDetailsEl.append(moviePlot);
 
-  let movieGross = document.createElement("p"); 
-  movieGross.textContent = "Box Office: " + movieData.BoxOffice; 
-  movDetailsEl.append(movieRating); 
-  
+  let movieCast = document.createElement("p");
+  movieCast.textContent = "Cast: " + movieData.Actors;
+  movDetailsEl.append(movieCast);
+
+  let movieRating = document.createElement("p");
+  movieRating.textContent = "Rated: " + movieData.Rated;
+  movDetailsEl.append(movieRating);
+
+  let movieGross = document.createElement("p");
+  movieGross.textContent = "Box Office: " + movieData.BoxOffice;
+  movDetailsEl.append(movieRating);
+
 }
